066c4323a88b57af043cce20ce4de345e0832925
Catarina Cepeda <catarinacepeda@gmail.com>
2016-11-07 00:38:13 +0000

    Add mobile app base

---
+++ SmartRestaurantApp/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/androidtemplate/ui/menu/MenuItemDetailActivity.java
@@ -0,0 +1,33 @@
+package pt.ulisboa.tecnico.meic.sirs.androidtemplate.ui.menu;
+
+import android.os.Bundle;
+
+import pt.ulisboa.tecnico.meic.sirs.androidtemplate.R;
+import pt.ulisboa.tecnico.meic.sirs.androidtemplate.ui.base.BaseActivity;
+
+/**
+ * Simple wrapper for {@link MenuItemDetailFragment}
+ * This wrapper is only used in single pan mode (= on smartphones)
+ * Created by Andreas Schrade on 14.12.2015.
+ */
+public class MenuItemDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        // Show the Up button in the action bar.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        MenuItemDetailFragment fragment =  MenuItemDetailFragment.newInstance(getIntent().getStringExtra(MenuItemDetailFragment.ARG_ITEM_ID));
+        getSupportFragmentManager().beginTransaction().replace(R.id.article_detail_container, fragment).commit();
+    }
+
+    @Override
+    public boolean providesActivityToolbar() {
+        return false;
+    }
+}
---
+++ SmartRestaurantApp/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/androidtemplate/ui/menu/MenuListActivity.java
@@ -0,0 +1,154 @@
+package pt.ulisboa.tecnico.meic.sirs.androidtemplate.ui.menu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import pt.ulisboa.tecnico.meic.sirs.androidtemplate.R;
+import pt.ulisboa.tecnico.meic.sirs.androidtemplate.dummy.DummyContent;
+import pt.ulisboa.tecnico.meic.sirs.androidtemplate.ui.base.BaseActivity;
+import pt.ulisboa.tecnico.meic.sirs.androidtemplate.util.LogUtil;
+
+/**
+ * Lists all available quotes. This Activity supports a single pane (= smartphones) and a two pane mode (= large screens with >= 600dp width).
+ *
+ * Created by Andreas Schrade on 14.12.2015.
+ */
+public class MenuListActivity extends BaseActivity implements MenuListFragment.Callback {
+    /**
+     * Whether or not the activity is running on a device with a large screen
+     */
+    private boolean twoPaneMode;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    public static String POSITION = "POSITION";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        setupToolbar();
+
+        if (isTwoPaneLayoutUsed()) {
+            twoPaneMode = true;
+            LogUtil.logD("TEST","TWO POANE TASDFES");
+            enableActiveItemState();
+        }
+
+        if (savedInstanceState == null && twoPaneMode) {
+            setupDetailFragment();
+        }
+
+        // Get the ViewPager and set its PagerAdapter so that it can display items
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SlidingTabsAdapter(getSupportFragmentManager(), MenuListActivity.this));
+
+        // Give the TabLayout the ViewPager
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.go_to_shopping_cart);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(POSITION, tabLayout.getSelectedTabPosition());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        viewPager.setCurrentItem(savedInstanceState.getInt(POSITION));
+    }
+
+    /**
+     * Called when an item has been selected
+     *
+     * @param id the selected quote ID
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (twoPaneMode) {
+            // Show the quote detail information by replacing the DetailFragment via transaction.
+            MenuItemDetailFragment fragment = MenuItemDetailFragment.newInstance(id);
+            getSupportFragmentManager().beginTransaction().replace(R.id.article_detail_container, fragment).commit();
+        } else {
+            // Start the detail activity in single pane mode.
+            Intent detailIntent = new Intent(this, MenuItemDetailActivity.class);
+            detailIntent.putExtra(MenuItemDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+
+    private void setupToolbar() {
+        final ActionBar ab = getActionBarToolbar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupDetailFragment() {
+        MenuItemDetailFragment fragment =  MenuItemDetailFragment.newInstance(DummyContent.ITEMS.get(0).id);
+        getSupportFragmentManager().beginTransaction().replace(R.id.article_detail_container, fragment).commit();
+    }
+
+    /**
+     * Enables the functionality that selected items are automatically highlighted.
+     */
+    private void enableActiveItemState() {
+        MenuListFragment fragmentById = (MenuListFragment) getSupportFragmentManager().findFragmentById(R.id.article_list);
+        fragmentById.getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+    }
+
+    /**
+     * Is the container present? If so, we are using the two-pane layout.
+     *
+     * @return true if the two pane layout is used.
+     */
+    private boolean isTwoPaneLayoutUsed() {
+        return findViewById(R.id.article_detail_container) != null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                openDrawer();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected int getSelfNavDrawerItem() {
+        return R.id.nav_restaurant_menu;
+    }
+
+    @Override
+    public boolean providesActivityToolbar() {
+        return true;
+    }
+}
