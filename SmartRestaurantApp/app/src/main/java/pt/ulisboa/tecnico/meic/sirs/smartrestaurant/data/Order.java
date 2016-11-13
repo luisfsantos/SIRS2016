267a63a66babcad36336a6f515ff67bcd5a5c53e
Catarina Cepeda <catarinacepeda@gmail.com>
2016-11-13 14:50:17 +0000

    :iphone: Add quantity increase/decrease to order items

--- SmartRestaurantApp/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartrestaurant/data/Order.java
+++ SmartRestaurantApp/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartrestaurant/data/Order.java
@@ -1,26 +1,50 @@
 package pt.ulisboa.tecnico.meic.sirs.smartrestaurant.data;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
  * Created by Catarina on 12/11/2016.
  */
-
 public class Order {
 
     /**
      * Stores the IDs of the MenuItems added to order
      */
     public static List<String> ITEMS = new ArrayList<>();
+    /**
+     * Stores quantity for each MenuItem added to order.
+     * Key: Item ID; Value: quantity
+     */
+    private static Map<String, Integer> QUANTITY = new HashMap<>();
 
     public static void addItem(RestaurantMenu.MenuItem item) {
-        if (!ITEMS.contains(item.id))
+        if (!ITEMS.contains(item.id)) {
             ITEMS.add(item.id);
+            QUANTITY.put(item.id, 1);
+        }
+    }
+
+    public static void increaseItemQuantity(int position) {
+        String id = ITEMS.get(position);
+        QUANTITY.put(id, QUANTITY.get(id) + 1);
+    }
+
+    public static void decreaseItemQuantity(int position) {
+        String id = ITEMS.get(position);
+        if (QUANTITY.get(id) > 1)
+            QUANTITY.put(id, QUANTITY.get(id) - 1);
+    }
+
+    public static int getItemQuantity(int position) {
+        return QUANTITY.get(ITEMS.get(position));
     }
 
     public static void deleteItem(int position) {
+        QUANTITY.remove(ITEMS.get(position));
         ITEMS.remove(position);
     }
 }
--- SmartRestaurantApp/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartrestaurant/ui/menu/OrderListFragment.java
+++ SmartRestaurantApp/app/src/main/java/pt/ulisboa/tecnico/meic/sirs/smartrestaurant/ui/menu/OrderListFragment.java
@@ -11,36 +11,24 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
-import butterknife.OnClick;
 import pt.ulisboa.tecnico.meic.sirs.smartrestaurant.R;
 import pt.ulisboa.tecnico.meic.sirs.smartrestaurant.data.Order;
 import pt.ulisboa.tecnico.meic.sirs.smartrestaurant.data.RestaurantMenu;
-import pt.ulisboa.tecnico.meic.sirs.smartrestaurant.ui.base.BaseActivity;
-import pt.ulisboa.tecnico.meic.sirs.smartrestaurant.util.LogUtil;
-
-import static pt.ulisboa.tecnico.meic.sirs.smartrestaurant.util.LogUtil.makeLogTag;
 
 /**
  * Created by Catarina on 12/11/2016.
  */
 public class OrderListFragment extends ListFragment {
-    private static final String TAG = makeLogTag(BaseActivity.class);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setListAdapter(new OrderListFragment.OrderListAdapter());
         setHasOptionsMenu(true);
-    }
-
-    @OnClick(R.id.delete_item)
-    public void onDeleteItemClicked(View view) {
-
     }
 
     private class OrderListAdapter extends BaseAdapter {
@@ -60,6 +48,10 @@ public class OrderListFragment extends ListFragment {
             return RestaurantMenu.ITEM_MAP.get(Order.ITEMS.get(position)).id.hashCode();
         }
 
+        public String getItemQuantity(int position) {
+            return Integer.toString(Order.getItemQuantity(position));
+        }
+
         @Override
         public View getView(final int position, View convertView, ViewGroup container) {
             if (convertView == null) {
@@ -68,7 +60,7 @@ public class OrderListFragment extends ListFragment {
 
             final RestaurantMenu.MenuItem item = (RestaurantMenu.MenuItem) getItem(position);
             ((TextView) convertView.findViewById(R.id.menu_item_name)).setText(item.title);
-            ((TextView) convertView.findViewById(R.id.menu_item_price)).setText(item.year);
+            ((TextView) convertView.findViewById(R.id.menu_item_price)).setText(getItemQuantity(position) + " x " + item.year);
             final ImageView img = (ImageView) convertView.findViewById(R.id.thumbnail);
 
             Glide.with(getActivity()).load(item.posterURL).asBitmap().fitCenter().into(new BitmapImageViewTarget(img) {
@@ -79,11 +71,28 @@ public class OrderListFragment extends ListFragment {
                     img.setImageDrawable(circularBitmapDrawable);
                 }
             });
+
             convertView.findViewById(R.id.delete_item).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    LogUtil.logD(TAG, "Deleted item " + RestaurantMenu.ITEM_MAP.get(Order.ITEMS.get(position)).title + " from order.");
                     Order.deleteItem(position);
+                    notifyDataSetChanged();
+                }
+            });
+
+            convertView.findViewById(R.id.quantity_up).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Order.increaseItemQuantity(position);
+                    notifyDataSetChanged();
+                }
+            });
+
+            convertView.findViewById(R.id.quantity_down).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Order.decreaseItemQuantity(position);
+                    notifyDataSetChanged();
                 }
             });
 
