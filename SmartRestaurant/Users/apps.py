c09f23cf3aa0336f50e816b3d71ed2f065d063f8
Luis Santos <luis.fa.santos@tecnico.ulisboa.pt>
2016-11-16 12:04:45 +0000

    :computer: add custom user

--- SmartRestaurant/Users/apps.py
+++ SmartRestaurant/Users/apps.py
@@ -5,3 +5,10 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'Users'
+
+class ProfilesConfig(AppConfig):
+    name = 'SmartRestaurant.Users'
+    verbose_name = _('customer')
+
+    def ready(self):
+        import Users.signals
--- SmartRestaurant/Users/models.py
+++ SmartRestaurant/Users/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Employee(models.Model):
@@ -9,4 +10,4 @@ class Employee(models.Model):
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nib = models.IntegerField()
+    nif = models.IntegerField()
