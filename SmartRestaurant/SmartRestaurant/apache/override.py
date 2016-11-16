be376f5a32e991a962eff9d35a54cdb784abb1a5
root <root@vps312496.ovh.net>
2016-11-17 00:21:52 +0100

    :computer: add apache config

---
+++ SmartRestaurant/SmartRestaurant/apache/override.py
@@ -0,0 +1,6 @@
+
+
+from SmartRestaurant.settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['www.luissantos.me', 'luissantos.me']
--- SmartRestaurant/SmartRestaurant/wsgi.py
+++ SmartRestaurant/SmartRestaurant/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+os.environ['PYTHON_EGG_CACHE'] = '/opt/.python-eggs'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SmartRestaurant.settings")
 
 application = get_wsgi_application()
