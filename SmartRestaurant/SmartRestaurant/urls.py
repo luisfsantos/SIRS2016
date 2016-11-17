e15a0868cfbd2875e9991586da27993a98cf8c0b
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-11-17 15:16:59 +0000

    :computer: fix menu

--- SmartRestaurant/SmartRestaurant/urls.py
+++ SmartRestaurant/SmartRestaurant/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('Users.urls')),
-    url(r'^menu', include('Menu.urls')),
+    url(r'^menu/', include('Menu.urls')),
 ]
