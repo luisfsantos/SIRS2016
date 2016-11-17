c706b782683923c5a17b44df28b79431b01b0b67
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-11-17 17:54:46 +0000

    :computer: fix menu

--- SmartRestaurant/Menu/serializers.py
+++ SmartRestaurant/Menu/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ('name')
+        fields = ('name',)
 
     def create(self, validated_data):
         ingredient = Ingredient(
