41904cb377d2a7d2796a25c2d7b24160213aa72a
Luis Santos <luis.fa.santos@tecnico.ulisboa.pt>
2016-11-27 20:09:50 +0000

    :computer: ingredients

--- SmartRestaurant/Menu/migrate_menu.py
+++ SmartRestaurant/Menu/migrate_menu.py
@@ -1,23 +1,23 @@
 from Menu.models import Meal, Ingredient
 from django.core.files import File
-# ingredient_list = ["pineapple", "chestnut", "bacon", "sesame-ginger salad dressing", "green onion", "kiwi",
-#                    "golden apple", "raspberry", "strawberry", "white sugar", "brown sugar", "tortillas", "cinnamon sugar",
-#                    "vegetable oil", "chicken breast", "red bell pepper", "corn", "black beans", "spinach", "jalapeno peppers",
-#                    "parsley", "ground cumin", "chilly powder", "salt", "Monterey Jack cheese", "red wine vinegar",
-#                    "lime", "ground black pepper", "garlic", "dried oregano", "tomato", "lettuce", "salsa", "spaghetti",
-#                    "olive oil", "onion", "white wine", "eggs", "parmesan", "yeast", "flour", "tomato paste",
-#                    "onion powder", "kosher salt","mozzarella", "mushrooms","milk chocolate","white chocolate", "shortening",
-#                    "half-and-half cream","heavy cream","vanilla extract", "ground cinnamon", "graham crackers","butter",
-#                    "cream cheese", "milk", "sour cream"]
-#
-# for ingredient in ingredient_list:
-#     Ingredient.objects.create(name=ingredient)
+ingredient_list = ["pineapple", "chestnut", "bacon", "sesame-ginger salad dressing", "green onion", "kiwi",
+                   "golden apple", "raspberry", "strawberry", "white sugar", "brown sugar", "tortillas", "cinnamon sugar",
+                   "vegetable oil", "chicken breast", "red bell pepper", "corn", "black beans", "spinach", "jalapeno peppers",
+                   "parsley", "ground cumin", "chilly powder", "salt", "Monterey Jack cheese", "red wine vinegar",
+                   "lime", "ground black pepper", "garlic", "dried oregano", "tomato", "lettuce", "salsa", "spaghetti",
+                   "olive oil", "onion", "white wine", "eggs", "parmesan", "yeast", "flour", "tomato paste",
+                   "onion powder", "kosher salt","mozzarella", "mushrooms","milk chocolate","white chocolate", "shortening",
+                   "half-and-half cream","heavy cream","vanilla extract", "ground cinnamon", "graham crackers","butter",
+                   "cream cheese", "milk", "sour cream"]
+
+for ingredient in ingredient_list:
+    Ingredient.objects.create(name=ingredient)
 
 #Meal.objects.create(meal_type=,name=, price=, description=,calories=,)
 
 meal = Meal(meal_type="DE",name="Chocolate Covered Strawberries", price=6.99,
                     description="Eight fresh strawberries dipped in delicious milk chocolate (from Sweden) with white "
-                                "chocolate drizzled finely over them!",calories=115)
+                                "chocolate drizzled finely over them!",calories=115, image_url="https://luissantos.me/media/strawberry")
 
 meal.save()
 
