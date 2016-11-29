c059de4184fa7b4a828e676fc180575d75085b2f
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-11-29 18:32:39 +0000

    :computer: add login forms

--- SmartRestaurant/Users/forms.py
+++ SmartRestaurant/Users/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from Users.models import UserProfile
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
-class UserForm(forms.Form):
+class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['email', 'username', 'password', 'first_name', 'last_name']
 
-class UserProfileForm(forms.Form):
+class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = ['nif',]
