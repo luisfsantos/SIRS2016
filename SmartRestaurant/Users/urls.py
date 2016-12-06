ec44e17fa607434a4575716f93ce42678da838f7
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-12-06 16:28:55 +0000

    :computer: add home

--- SmartRestaurant/Users/urls.py
+++ SmartRestaurant/Users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 from Users import views
 from Users import APIviews
@@ -7,7 +8,8 @@ app_name='accounts'
 urlpatterns = [
     url(r'^accounts/$', APIviews.AccountListAPI),
     url(r'^register/$', views.register_staff),
-    url(r'^login/$', views.login_staff),
+    url(r'^login/$', views.login_staff, name = 'login'),
+    url(r'^/$', RedirectView.as_view(pattern_='login')),
     url(r'^logout/$', views.logout_user, name='logout'),
     url(r'^test/$', APIviews.test_loggedinAPI),
 ]
