00649439a91780097d3c7925be81f67a6b755e44
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2016-12-06 19:17:30 +0000

    :computer: logging clear table

--- SmartRestaurant/SmartRestaurant/apache/override.py
+++ SmartRestaurant/SmartRestaurant/apache/override.py
@@ -1,7 +1,74 @@
-
-
 from SmartRestaurant.settings import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['www.luissantos.me', 'luissantos.me']
 PRODUCTION = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'simple': {
+            'format': '[%(asctime)s] %(levelname)s %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'verbose': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'development_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/django_dev.log',
+            'formatter': 'verbose'
+        },
+        'production_logfile': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django_production.log',
+            'formatter': 'simple'
+        },
+        'dba_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_false', 'require_debug_true'],
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django_dba.log',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'POS': {
+            'handlers': ['console', 'development_logfile', 'production_logfile'],
+        },
+        'Tables': {
+            'handlers': ['console', 'development_logfile', 'production_logfile'],
+        },
+        'dba': {
+            'handlers': ['console', 'dba_logfile'],
+        },
+        'django': {
+            'handlers': ['console', 'development_logfile', 'production_logfile'],
+        },
+        'py.warnings': {
+            'handlers': ['console', 'development_logfile'],
+        },
+    }
+}
--- SmartRestaurant/Tables/views.py
+++ SmartRestaurant/Tables/views.py
@@ -2,25 +2,43 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from Tables.models import TableModel
 from Tables.forms import CleanTableForm
+
+# Python logging package
+import logging
+import uuid
+
+# Standard instance of a logger with __name__
+stdlogger = logging.getLogger(__name__)
+
+
 # Create your views here.
 def clean_table(request):
     """
     Register Staff to the System
     """
-    if request.user.groups.filter(name='staff') or request.user.is_superuser:
-        if request.method == "POST":
-            ctform = CleanTableForm(data=request.POST)
-            if ctform.is_valid():
-                try:
-                    table = TableModel.objects.get(table_id = ctform.cleaned_data.get('table_id'))
-                    table.user = None
-                    for order in table.order_set.all():
-                        order.status = 'AR'
-                        order.save()
-                    table.save()
-                except TableModel.DoesNotExist:
-                    redirect('/pos/view')
+    if not request.user.is_anonymous:
+        if request.user.groups.filter(name='staff') or request.user.is_superuser:
+            if request.method == "POST":
+                ctform = CleanTableForm(data=request.POST)
+                if ctform.is_valid():
+                    try:
+                        table = TableModel.objects.get(table_id=ctform.cleaned_data.get('table_id'))
+                        id = uuid.uuid4() + ": "
+                        stdlogger.warning(id + "The table: " + ctform.cleaned_data.get('table_id') + " is getting cleaned by "
+                                          + request.user.get_full_name() + " " + request.user.userprofile.nif)
+                        table.user = None
 
-        return redirect('/pos/view')
+                        stdlogger.warning(id + "This affects:")
+                        for order in table.order_set.all():
+                            stdlogger.warning(id + "Order: " + order.identifier + " which has status " + order.get_status_display())
+                            order.status = 'AR'
+                            order.save()
+                        table.save()
+                    except TableModel.DoesNotExist:
+                        redirect('/pos/view')
+
+            return redirect('/pos/view')
+        else:
+            raise Http404
     else:
         raise Http404
