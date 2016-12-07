fc4a860acefd505190a433069c48a8ea646e291e
Luis Santos <luis.fa.santos@tecnico.ulisboa.pt>
2016-12-07 16:55:10 +0000

    :computer: add visualization to order

--- SmartRestaurant/POS/urls.py
+++ SmartRestaurant/POS/urls.py
@@ -6,7 +6,10 @@ app_name='POS'
 
 urlpatterns = [
     url(r'^view/$', views.posView, name='view'),
-    url(r'^view/order/$', views.view_user_info),
+    url(r'^view/order/user/$', views.view_user_info),
+    url(r'^view/order/$', views.view_order_info),
+    url(r'^order/edit/processed/$', views.process_order),
+    url(r'^order/pay/cash/$', views.receive_cash_order),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- SmartRestaurant/POS/views.py
+++ SmartRestaurant/POS/views.py
@@ -3,10 +3,7 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import Http404, HttpResponseRedirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required, user_passes_test
-
+from django.http import Http404
 from Orders.models import UserOrders, Order
 from POS.forms import OrderIDForm
 from Tables.models import TableModel
@@ -42,6 +39,10 @@ def view_user_info(request):
         messages.add_message(request, messages.INFO, "You can't access the POS console if you aren't logged in.")
         return redirect('/login')
     else:
+        whois_name = str(
+            request.user.get_full_name()) if request.user.get_full_name() is not None else request.user.username
+        whois_id = str(
+            request.user.userprofile.nif) if request.user.userprofile.nif is not None else request.user.email
         if request.user.is_superuser:
             if request.method == "POST":
                 form = OrderIDForm(data=request.POST)
@@ -49,19 +50,118 @@ def view_user_info(request):
                     try:
                         user_order = UserOrders.objects.get(order=form.cleaned_data.get('identifier'))
                         order = Order.objects.get(identifier=form.cleaned_data.get('identifier'))
+                        stdlogger.info("The user: " + whois_name + " " + whois_id + " viewed the client info of order " + str(form.cleaned_data.get('identifier')))
                         return render(request, 'POS/clientinfo.html',
                                {'staff_name': request.user.first_name + " " + request.user.last_name,
                                 'user_order': user_order,
                                 'order': order})
                     except UserOrders.DoesNotExist:
+                        stdlogger.info("The user: " + whois_name + " " + whois_id + " tried to view the client info of order " + str(
+                            form.cleaned_data.get('identifier')))
                         redirect('/pos/view')
             else:
                 return redirect('/pos/view')
         else:
             messages.add_message(request, messages.INFO, "You do not have permission to view data.")
-            whois_name = str(
-                request.user.get_full_name()) if request.user.get_full_name() is not None else request.user.username
-            whois_id = str(
-                request.user.userprofile.nif) if request.user.userprofile.nif is not None else request.user.email
+            stdlogger.warning("The user: " + whois_name + " " + whois_id + " attempted to view user data from order.")
+            return redirect('/login')
+
+def view_order_info(request):
+    if request.user.is_anonymous:
+        messages.add_message(request, messages.INFO, "You can't access the POS console if you aren't logged in.")
+        return redirect('/login')
+    else:
+        whois_name = str(
+            request.user.get_full_name()) if request.user.get_full_name() is not None else request.user.username
+        whois_id = str(
+            request.user.userprofile.nif) if request.user.userprofile.nif is not None else request.user.email
+        if request.user.groups.filter(name='staff') or request.user.is_superuser:
+            if request.method == "POST":
+                form = OrderIDForm(data=request.POST)
+                if form.is_valid():
+                    try:
+                        UserOrders.objects.get(order=form.cleaned_data.get('identifier'))
+                        order = Order.objects.get(identifier=form.cleaned_data.get('identifier'))
+                        stdlogger.info("The user: " + whois_name + " " + whois_id + " viewed order " + str(form.cleaned_data.get('identifier')))
+                        return render(request, 'POS/orderinfo.html',
+                               {'staff_name': request.user.first_name + " " + request.user.last_name,
+                                'order': order})
+                    except UserOrders.DoesNotExist:
+                        stdlogger.info("The user: " + whois_name + " " + whois_id + " tried to view order " + str(
+                            form.cleaned_data.get('identifier')))
+                        redirect('/pos/view')
+            else:
+                return redirect('/pos/view')
+        else:
+            messages.add_message(request, messages.INFO, "You do not have permission to view data.")
             stdlogger.warning("The user: " + whois_name + " " + whois_id + " attempted to view user data.")
             return redirect('/login')
+
+
+def process_order(request):
+    if request.user.is_anonymous:
+        raise Http404
+    else:
+        whois_name = str(
+            request.user.get_full_name()) if request.user.get_full_name() is not None else request.user.username
+        whois_id = str(
+            request.user.userprofile.nif) if request.user.userprofile.nif is not None else request.user.email
+        if request.user.groups.filter(name='staff') or request.user.is_superuser:
+            if request.method == "POST":
+                form = OrderIDForm(data=request.POST)
+                if form.is_valid():
+                    try:
+                        order = Order.objects.get(identifier=form.cleaned_data.get('identifier'))
+                        if order.payment == 'CF':
+                            order.status = 'DE'
+                            order.save()
+                            stdlogger.info("The user: " + whois_name + " " + whois_id + " delivered " + str(form.cleaned_data.get('identifier')))
+                            return redirect('/pos/view')
+                        else:
+                            stdlogger.info("The user: " + whois_name + " " + whois_id + " tried to deliver the order " + str(
+                                form.cleaned_data.get('identifier')) + " but it was not payed for")
+                            return redirect('/pos/view')
+                    except Order.DoesNotExist:
+                        stdlogger.info("The user: " + whois_name + " " + whois_id + " tried to deliver the order " + str(
+                            form.cleaned_data.get('identifier')))
+                        redirect('/pos/view')
+            else:
+                return redirect('/pos/view')
+        else:
+            stdlogger.warning("The user: " + whois_name + " " + whois_id + " attempted to deliver an order")
+            raise Http404
+
+
+def receive_cash_order(request):
+    if request.user.is_anonymous:
+        raise Http404
+    else:
+        whois_name = str(
+            request.user.get_full_name()) if request.user.get_full_name() is not None else request.user.username
+        whois_id = str(
+            request.user.userprofile.nif) if request.user.userprofile.nif is not None else request.user.email
+        if request.user.groups.filter(name='staff') or request.user.is_superuser:
+            if request.method == "POST":
+                form = OrderIDForm(data=request.POST)
+                if form.is_valid():
+                    try:
+                        order = Order.objects.get(identifier=form.cleaned_data.get('identifier'))
+                        if order.payment == 'PE' and order.cash == 'FE':
+                            order.payment = 'CF'
+                            order.cash = 'RE'
+                            order.save()
+                            stdlogger.info("The user: " + whois_name + " " + whois_id + " fetched the money for order " + str(form.cleaned_data.get('identifier')))
+                            return redirect('/pos/view')
+                        else:
+                            stdlogger.info("The user: " + whois_name + " " + whois_id + " accept payment for the order " + str(
+                                form.cleaned_data.get('identifier')) + " but it was not possible")
+                            return redirect('/pos/view')
+                    except Order.DoesNotExist:
+                        stdlogger.info("The user: " + whois_name + " " + whois_id + " tried to pay for the order " + str(
+                            form.cleaned_data.get('identifier')))
+                        redirect('/pos/view')
+            else:
+                return redirect('/pos/view')
+        else:
+            stdlogger.warning("The user: " + whois_name + " " + whois_id + " attempted to deliver an order")
+            raise Http404
